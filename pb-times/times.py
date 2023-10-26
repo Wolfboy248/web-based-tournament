0102f1fe9f38345ea12ad41625ffb6cf34f56238
alexinyew <csimmons11121@gmail.com>
2023-10-26 15:12:59 -0400

    setmap, setpb console aliases, decimal places fix

--- pb-times/times.py
+++ pb-times/times.py
@@ -1,21 +1,23 @@
-import telnetlib3, asyncio, sys, json, time, os
+import telnetlib3, asyncio, sys, json, os
 
-if len(sys.argv) < 5:
-    print('Usage: [port] [map name] [player one] [player two]')
+if len(sys.argv) < 4:
+    print('Usage: [port] [player one] [player two]')
     exit()
 
-print('notes:\n  ensure portal 2 is running with the launch option \'-netconport [port]\'')
-print('  you may want to bind something to \"echo advance round\" to start saving times to the next round.')
+print('notes:\n  ensure portal 2 is running with the launch option \"-netconport [port]\"')
+print('  you may want to bind something to the \"advance\" alias to advance to the next round!')
+print('  use the \"setpb\" alias to override someones time on a round, ex: \"setpb alexz 1 20.30\"')
+print('  use the \"setmap\" alias to set the map, ex: \"setmap sp_a1_intro4\"')
 print('  when this script runs the times in the json are wiped!')
 
-port, currentMap, p1, p2 = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-currentRound = 1
+port, p1, p2 = sys.argv[1], sys.argv[2], sys.argv[3]
+currentRound, currentMap = 1, ''
 info_path = os.path.join(os.path.dirname(__file__), '../maps/info.json')
 
 info = {
     'player1': p1,
     'player2': p2,
-    'round1P1PB': '9999',
+    'round1P1PB': '9999', # frontend should not show 9999
     'round1P2PB': '9999',
     'round2P1PB': '9999',
     'round2P2PB': '9999',
@@ -25,21 +27,25 @@ info = {
     'current_map': currentMap
 }
 
-with open(info_path, 'w') as outfile:
-    outfile.write(json.dumps(info, indent=4))
+def write():
+    with open(info_path, 'w') as outfile:
+        outfile.write(json.dumps(info, indent=4))
 
 def unformatTime(text: str):
     if text.find(':') != -1:
         return int(text.split(':')[0]) * 60 + float(text.split(':')[1])
     return float(text)
 
-def formatTime(num: float):
-    return time.strftime('%-M:%S', num) + '.00' if num.is_integer() else ''
-
 async def shell(reader: telnetlib3.TelnetReader, writer: telnetlib3.TelnetWriter):
     global currentRound
+    global currentMap
     console: str = ''
     print('\nConnected!\n')
+
+    writer.write('sar_alias advance \"echo advance round\"\n')
+    writer.write('sar_function setmap echo "setmap $1"\n')
+    writer.write('sar_function setpb echo "forcepb $1 $2 $3$4$5$6\n')
+    writer.write('ghost_disconnect; ghost_name SERVER; ghost_set_color FF0000; restart_level\n')
 
     while True:
         data = await reader.read(1024)
@@ -52,22 +58,31 @@ async def shell(reader: telnetlib3.TelnetReader, writer: telnetlib3.TelnetWriter
         if len(lines) > 1:
             for line in lines:
                 if 'has finished on' in line:
-                    split = line.split(' ')
-                    name, playerMap, time = split[0], split[4], ''.join(split[6::])
+                    split = line.split(' has finished on ')
+                    name, playerMap, time = split[0], split[1].split()[0], ''.join(split[1].split('in ')[1::])[:-1]
                     fTime = unformatTime(time)
 
                     if playerMap != currentMap:
                         continue
 
                     if name == p1 and fTime < unformatTime(info[f'round{str(currentRound)}P1PB']):
-                        print(f'New pb for {name}: ${time}')
+                        print(f'New pb for {name}: {time}')
                         info[f'round{str(currentRound)}P1PB'] = time
                     elif name == p2 and fTime < unformatTime(info[f'round{str(currentRound)}P2PB']):
                         print(f'New pb for {name}: {time}')
                         info[f'round{str(currentRound)}P2PB'] = time
 
-                    with open(info_path, 'w') as outfile:
-                        outfile.write(json.dumps(info, indent=4))
+                    write()
+                elif line.startswith('forcepb'):
+                    split = line.split()
+                    name, round, time = split[1], split[2], ''.join(split[3::])
+                    print(f'forced pb for {name}: {time}')
+                    if name == p1:
+                        info[f'round{round}P1PB'] = time
+                    elif name == p2:
+                        info[f'round{round}P2PB'] = time
+                    
+                    write()
                 elif line.startswith('advance round'):
                     if currentRound == 3:
                         print('already at round 3!')
@@ -75,6 +90,11 @@ async def shell(reader: telnetlib3.TelnetReader, writer: telnetlib3.TelnetWriter
 
                     currentRound += 1
                     print(f'advancing to round: {str(currentRound)}')
+                elif line.startswith('setmap'):
+                    currentMap = line.split()[1]
+                    info['current_map'] = currentMap
+                    print(f'set map to: {currentMap}\n')
+                    write()
 
 
         console = lines[-1]
