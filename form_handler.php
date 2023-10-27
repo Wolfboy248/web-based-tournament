5a6b48339cc3f40dd9770d4651bd6611d5901677
Wolfboy248 <121288977+Wolfboy248@users.noreply.github.com>
2023-10-27 14:48:09 +0200

    27/10/2023 - please fix :)

--- form_handler.php
+++ form_handler.php
@@ -7,21 +7,21 @@
     <body>
 
         Thank you for submitting!<br>
-        Your submitted vetoed maps are: <br> <?php echo $_POST["P1map1"]; ?><br>
-        <?php echo $_POST["P1map2"]; ?><br>
-        <?php echo $_POST["P1map3"]; ?><br>
-        <?php echo $_POST["P1map4"]; ?><br>
-        <?php echo $_POST["P1map5"]; ?><br>
+        Your submitted vetoed maps are: <br> <?php echo $_GET["P1map1"]; ?><br>
+        <?php echo $_GET["P1map2"]; ?><br>
+        <?php echo $_GET["P1map3"]; ?><br>
+        <?php echo $_GET["P1map4"]; ?><br>
+        <?php echo $_GET["P1map5"]; ?><br>
         <?php 
         
         $path = 'maps/vetoesP1.json';
         
         $jsonData = [
-                "P1Map1" => $_POST["P1map1"],
-                "P1Map2" => $_POST["P1map2"],
-                "P1Map3" => $_POST["P1map3"],
-                "P1Map4" => $_POST["P1map4"],
-                "P1Map5" => $_POST["P1map5"]
+                "P1Map1" => $_GET["P1map1"],
+                "P1Map2" => $_GET["P1map2"],
+                "P1Map3" => $_GET["P1map3"],
+                "P1Map4" => $_GET["P1map4"],
+                "P1Map5" => $_GET["P1map5"]
             ];
 
         $jsonString = json_encode($jsonData, JSON_PRETTY_PRINT);
--- form_handler2.php
+++ form_handler2.php
@@ -7,21 +7,21 @@
     <body>
 
         Thank you for submitting!<br>
-        Your submitted vetoed maps are: <br> <?php echo $_POST["P2map1"]; ?><br>
-        <?php echo $_POST["P2map2"]; ?><br>
-        <?php echo $_POST["P2map3"]; ?><br>
-        <?php echo $_POST["P2map4"]; ?><br>
-        <?php echo $_POST["P2map5"]; ?><br>
+        Your submitted vetoed maps are: <br> <?php echo $_GET["P2map1"]; ?><br>
+        <?php echo $_GET["P2map2"]; ?><br>
+        <?php echo $_GET["P2map3"]; ?><br>
+        <?php echo $_GET["P2map4"]; ?><br>
+        <?php echo $_GET["P2map5"]; ?><br>
         <?php 
         
         $path = 'maps/vetoesP2.json';
         
         $jsonData = [
-                "P2Map1" => $_POST["P2map1"],
-                "P2Map2" => $_POST["P2map2"],
-                "P2Map3" => $_POST["P2map3"],
-                "P2Map4" => $_POST["P2map4"],
-                "P2Map5" => $_POST["P2map5"]
+                "P2Map1" => $_GET["P2map1"],
+                "P2Map2" => $_GET["P2map2"],
+                "P2Map3" => $_GET["P2map3"],
+                "P2Map4" => $_GET["P2map4"],
+                "P2Map5" => $_GET["P2map5"]
             ];
 
         $jsonString = json_encode($jsonData, JSON_PRETTY_PRINT);
