1318f1ea639b8fb06917c8e59ca810a456cef0bc
FifthWit <110744759+FifthWit@users.noreply.github.com>
2023-10-28 01:16:04 +0000

    added veto checking to make sure there was not a typo 🥰🥰🥰

--- main.js
+++ main.js
@@ -8,6 +8,37 @@ async function main() {
     document.querySelector("#btnP2").innerText = info.player2;
 }
 
+const Data = new P2Data()
+
+function validate() {
+    let valid = false; // Initialize valid as false
+
+    for (let inputs = 0; inputs < 5; inputs++) {
+        for (let maps = 0; maps < Data.maps.length; maps++) {
+            try {
+                if (document.getElementById(`P1map${inputs+1}INP`).value == Data.maps[maps].splitname) {
+                    console.log(inputs)
+                    valid = true;
+                    break; // Exit the inner loop when a match is found
+                }
+            } catch (error) {
+                console.log(error)
+            }
+            //console.log(maps, Data.maps[maps].splitname, document.getElementById(`P1map${inputs+1}INP`).value)
+        }
+        
+        if (!valid) {
+            alert(`You have an invalid vetoe on Map ${inputs+1}`);
+            
+            return false;
+        }
+
+    }
+
+    return valid;
+}
+
+
 function player1Click() {
     document.querySelector("#mainContainer").style.transform = "translate(-50%, -400%)";
     document.getElementById("P1Form").style.transform = "translate(-50%, -50%)";
@@ -25,5 +56,19 @@ document.querySelector("#btnP1").addEventListener('click', () => {
 document.querySelector("#btnP2").addEventListener('click', () => {
     player2Click();
 });
+
+function player1submit() {
+    if (validate()) {
+        document.getElementById('form1').submit()
+    }
+}
+
+function player2submit() {
+    if (validate()) {
+        document.getElementById('form2').submit();
+    }
+
+}
+
 
 main();
