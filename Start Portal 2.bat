8a2a586841bafa6d6f1e7c05979ced4c4f566f7a
Wolfboy248 <121288977+Wolfboy248@users.noreply.github.com>
2024-02-02 13:54:52 +0100

    update to the p2 batch file

--- Start Portal 2.bat
+++ Start Portal 2.bat
@@ -1,4 +1,4 @@
 @echo off
 taskkill /f /im portal2.exe
-timeout /t 6
-start "" "steam://rungameid/620// -netconport 60 -novid -textmode"
+timeout /t 3
+start "" "steam://rungameid/620// -netconport 60 -novid -textmode +plugin_load sar +ghost_name SERVER"
